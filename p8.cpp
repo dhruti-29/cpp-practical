dfe5ee6f8388f19ca936557dd7bef9947dbd4624
dhruti viradiya <25cs113@charusat.edu.in>
2026-04-23 20:48:01 +0530

    add new code file

---
+++ p8.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+/* 
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+
+
+
+*/
+class p{
+public:
+
+void patteren3(int n){
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<2*i + 1;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+
+ for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<2*n-2*i-1;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+};
+
+int main() {
+    p p3;
+    p3.patteren3(5);
+    return 0;
+}
